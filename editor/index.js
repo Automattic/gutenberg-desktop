f34a794103ed4ce444203dd0ea420507efb3810d
John Godley <johng75@gmail.com>
2019-10-16 13:46:32 +0100

    Switch to the playground editor

--- editor/index.js
+++ editor/index.js
@@ -1,22 +1,63 @@
 /**
- * External dependencies
+ * WordPress dependencies
  */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { useState } from '@wordpress/element';
+import '@wordpress/editor'; // This shouldn't be necessary
+
+import { render, useState } from '@wordpress/element';
+import {
+	BlockEditorKeyboardShortcuts,
+	BlockEditorProvider,
+	BlockList,
+	WritingFlow,
+	ObserveTyping,
+} from '@wordpress/block-editor';
+import {
+	Popover,
+	SlotFillProvider,
+} from '@wordpress/components';
+import { registerCoreBlocks } from '@wordpress/block-library';
+import '@wordpress/format-library';
 
 /**
  * Internal dependencies
  */
-import './gutenbridge';
-import Editor from './component/editor';
 
-function App() {
+/* eslint-disable no-restricted-syntax */
+import '@wordpress/components/build-style/style.css';
+import '@wordpress/block-editor/build-style/style.css';
+import '@wordpress/block-library/build-style/style.css';
+import '@wordpress/block-library/build-style/editor.css';
+import '@wordpress/block-library/build-style/theme.css';
+import '@wordpress/format-library/build-style/style.css';
+/* eslint-enable no-restricted-syntax */
+
+function App( props ) {
 	const [ blocks, updateBlocks ] = useState( [] );
 
 	return (
-		<Editor blocks={ blocks } updateBlocks={ updateBlocks } />
+		<div className="playground__body">
+			<SlotFillProvider>
+				<BlockEditorProvider
+					value={ blocks }
+					onInput={ updateBlocks }
+					onChange={ updateBlocks }
+				>
+					<div className="editor-styles-wrapper">
+						<BlockEditorKeyboardShortcuts />
+						<WritingFlow>
+							<ObserveTyping>
+								<BlockList />
+							</ObserveTyping>
+						</WritingFlow>
+					</div>
+					<Popover.Slot />
+				</BlockEditorProvider>
+			</SlotFillProvider>
+		</div>
 	);
 }
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerCoreBlocks();
+
+render( <App />, document.getElementById( 'root' ) );
